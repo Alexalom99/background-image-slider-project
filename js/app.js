1326de95b18f4636679a84ddd9453af93026235f
Alexalom99 <alexalom1999@gmail.com>
2025-03-28 10:42:54 +0100

    done

--- js/app.js
+++ js/app.js
@@ -7,3 +7,46 @@ const pictures = [
 ];
 
 // buena suerte
+
+let imagecont = document.querySelector(".img-container");
+let index = 0;
+
+
+function changeimg() {
+  imagecont.style.background = `url(./img/${pictures[index]}.jpeg) center / cover fixed no-repeat`; 
+}
+
+
+const buttons = document.querySelectorAll(".btn");
+
+function goright() {
+  if (index == pictures.length - 1) {
+    index = 0;
+  }
+  else {
+    index++;
+  }
+} 
+
+function goleft() {
+  if (index == 0) {
+    index = pictures.length-1;
+  }
+  else {
+    index--;
+  }
+}
+
+
+buttons.forEach(function(button){
+  button.addEventListener("click",function() {
+    if (button.classList.contains("btn-left")) {
+      goleft();
+      changeimg();
+    }
+    else if(button.classList.contains("btn-right")) {
+      goright();
+      changeimg();
+    }
+  })
+})
